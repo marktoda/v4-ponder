3d633bbe9a1fa96c7fd0ffca135e1bf35a63be86
saucepoint <saucepoint@protonmail.com>
2025-02-11 01:11:53 -0500

    inline comments

--- src/index.ts
+++ src/index.ts
@@ -30,8 +30,11 @@ ponder.on("PoolManager:Swap", async ({ event, context }) => {
 });
 
 ponder.on("PoolManager:ModifyLiquidity", async ({ event, context }) => {
+  // determine the positionId, the same hash used by PoolManager / Position.sol
   const positionId = keccak256(encodePacked([event.args.sender, event.args.tickLower, event.args.tickUpper, event.args.salt], ["address", "int24", "int24", "bytes32"]));
 
+  // upsert into `position` table
+  // for cases where the position exists, update the position's liquidity according to the event
   await context.db.insert(schema.position).values({
     positionId: positionId,
     poolId: event.args.id,
