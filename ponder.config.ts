23e4c7f8f675671cedb46031bf1d383864095b16
Mark Toda <toda.mark@gmail.com>
2025-08-05 18:59:13 -0400

    feat: base only

--- ponder.config.ts
+++ ponder.config.ts
@@ -63,54 +63,54 @@ export default createConfig({
   contracts: {
     PoolManager: {
       chain: {
-        mainnet: {
-          address: "0x000000000004444c5dc75cb358380d2e3de08a90",
-          startBlock: 21688329
-        },
-        unichain: {
-          address: "0x1f98400000000000000000000000000000000004",
-          startBlock: 0
-        },
-        arbitrum: {
-          address: "0x360e68faccca8ca495c1b759fd9eee466db9fb32",
-          startBlock: 297842872,
-        },
-        optimism: {
-          address: "0x9a13f98cb987694c9f086b1f5eb990eea8264ec3",
-          startBlock: 130947675,
-        },
+        // mainnet: {
+        //   address: "0x000000000004444c5dc75cb358380d2e3de08a90",
+        //   startBlock: 21688329
+        // },
+        // unichain: {
+        //   address: "0x1f98400000000000000000000000000000000004",
+        //   startBlock: 0
+        // },
+        // arbitrum: {
+        //   address: "0x360e68faccca8ca495c1b759fd9eee466db9fb32",
+        //   startBlock: 297842872,
+        // },
+        // optimism: {
+        //   address: "0x9a13f98cb987694c9f086b1f5eb990eea8264ec3",
+        //   startBlock: 130947675,
+        // },
         base: {
           address: "0x498581ff718922c3f8e6a244956af099b2652b2b",
           startBlock: 25350988,
         },
-        polygon: {
-          address: "0x67366782805870060151383f4bbff9dab53e5cd6",
-          startBlock: 66980384,
-        },
-        worldchain: {
-          address: "0xb1860d529182ac3bc1f51fa2abd56662b7d13f33",
-          startBlock: 9111872,
-        },
-        blast: {
-          address: "0x1631559198a9e474033433b2958dabc135ab6446",
-          startBlock: 14377311,
-        },
-        ethereum_sepolia: {
-          address: "0xE03A1074c86CFeDd5C142C4F04F1a1536e203543",
-          startBlock: 7258946
-        },
-        unichain_sepolia: {
-          address: "0x00B036B58a818B1BC34d502D3fE730Db729e62AC",
-          startBlock: 7092034
-        },
-        base_sepolia: {
-          address: "0x05E73354cFDd6745C338b50BcFDfA3Aa6fA03408",
-          startBlock: 19088197,
-        },
-        arbitrum_sepolia: {
-          address: "0xFB3e0C6F74eB1a21CC1Da29aeC80D2Dfe6C9a317",
-          startBlock: 105909222,
-        }
+        // polygon: {
+        //   address: "0x67366782805870060151383f4bbff9dab53e5cd6",
+        //   startBlock: 66980384,
+        // },
+        // worldchain: {
+        //   address: "0xb1860d529182ac3bc1f51fa2abd56662b7d13f33",
+        //   startBlock: 9111872,
+        // },
+        // blast: {
+        //   address: "0x1631559198a9e474033433b2958dabc135ab6446",
+        //   startBlock: 14377311,
+        // },
+        // ethereum_sepolia: {
+        //   address: "0xE03A1074c86CFeDd5C142C4F04F1a1536e203543",
+        //   startBlock: 7258946
+        // },
+        // unichain_sepolia: {
+        //   address: "0x00B036B58a818B1BC34d502D3fE730Db729e62AC",
+        //   startBlock: 7092034
+        // },
+        // base_sepolia: {
+        //   address: "0x05E73354cFDd6745C338b50BcFDfA3Aa6fA03408",
+        //   startBlock: 19088197,
+        // },
+        // arbitrum_sepolia: {
+        //   address: "0xFB3e0C6F74eB1a21CC1Da29aeC80D2Dfe6C9a317",
+        //   startBlock: 105909222,
+        // }
       },
       abi: PoolManagerAbi,
       filter: [
